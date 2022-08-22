7a7123f5bb56a8f9514ca19b8adcc12ab886bca6
Ichim Emanuel <ichim.emy24@gmail.com>
2022-08-22 18:06:36 +0300

    prepare submit team, add $ shortcut to querySelector, use const instead of var, use arrow fn

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
+function $(selector) {
+  return document.querySelector(selector);
+}
+
 function getTeamHTML(team) {
   return `
   <tr>
-  <td>${team.promotion}</td>
-  <td>${team.members}</td>
-  <td>${team.name}</td>
-  <td>
-    <a href="${team.url}">open</a>
-  </td>
-  <td>x e</td>
-</tr>`;
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>
+      <a href="${team.url}">open</a>
+    </td>
+    <td>x e</td>
+  </tr>`;
 }
 
 function displayTeams(teams) {
-  // transforma in html
-  var teamsHTML = teams.map(getTeamHTML);
+  const teamsHTML = teams.map(getTeamHTML);
 
   // afisare
-  document.querySelector("table tbody").innerHTML = teamsHTML.join("");
+  $("table tbody").innerHTML = teamsHTML.join("");
 }
 
 function loadTeams() {
   fetch("data/teams.json")
-    .then(function (r) {
-      return r.json();
-    })
-    .then(function (teams) {
+    .then((r) => r.json())
+    .then((teams) => {
       displayTeams(teams);
     });
 }
 
+function submitForm(e) {
+  e.preventDefault();
+  const promotion = $("input[name=promotion]").value;
+  const members = $("input[name=members]").value;
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;
+
+  const team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+
+  console.warn("adauga in teams.json:", JSON.stringify(team));
+}
+
+function initEvents() {
+  const form = document.getElementById("editForm");
+  console.warn("form", form);
+  form.addEventListener("submit", submitForm);
+}
+
 loadTeams();
+initEvents();
